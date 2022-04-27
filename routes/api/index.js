9b2872e42ac8d5daa23857291464125dfd20434b
Zion Yang <zionremnant@gmail.com>
2022-04-26 23:10:35 -0700

    updated api routes index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const router = require("express").Router();
+const thoughtRoutes = require("./thoughtRoutes");
+const userRoutes = require("./userRoutes");
+const reactionRoutes = require("./reactionRoutes");
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use("/thoughts", thoughtRoutes);
+router.use("/users", userRoutes);
+router.use("/reactions", reactionRoutes);
 
 module.exports = router;
